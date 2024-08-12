56585c90a5dc544c2279e8e5f5e4d3a41ac5596e
Rakib Mahmud <fiverrrakib2017@gmail.com>
2024-08-12 16:14:42 +0600

    create feature bill collector

--- Sidebar_menu.php
+++ Sidebar_menu.php
@@ -127,6 +127,7 @@
                                     <li><a href="customers_payment.php">Customer Payment</a></li>
                                     <li><a href="recharge_filter.php">Recharge Filter</a></li>
                                     <li><a href="bill_collection.php">Bill Collection</a></li>
+                                    <li><a href="bill_collector.php">Bill Collector</a></li>
                                     <li><a href="mobile_banking.php">Mobile Banking Log</a></li>
                                     <li><a href="bkash_search.php">bKash Search</a></li>
                                 </ul>
