b91bc1b7315d527f59c6f116de7c903fbc72139e
Rakib Mahmud <fiverrrakib2017@gmail.com>
2024-12-03 13:40:16 +0600

    transaction table view area panel

--- branch/view_area.php
+++ branch/view_area.php
@@ -748,75 +748,7 @@ if ($pop_list = $con->query("SELECT * FROM add_pop WHERE id='$auth_usr_POP_id'")
                                     <div class="tab-pane p-3" id="pop_transaction" role="tabpanel">
                                         <div class="card">
                                             <div class="card-body">
-                                                <table id="transaction_datatable"
-                                                    class="table table-bordered dt-responsive nowrap"
-                                                    style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>ID</th>
-                                                            <th>Recharge Amount</th>
-                                                            <th>Paid Amount</th>
-                                                            <th>Action</th>
-                                                            <th>Transaction Type</th>
-                                                            <th>Date</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <?php
-                                                        $sql = "SELECT * FROM pop_transaction WHERE pop_id='$lstid'  ";
-                                                        $result = mysqli_query($con, $sql);
-
-                                                        while ($rows = mysqli_fetch_assoc($result)) {
-
-                                                        ?>
-
-                                                        <tr>
-                                                            <td><?php echo $rows['id']; ?></td>
-                                                            <td> <?php echo $rows['amount']; ?></td>
-                                                            <td> <?php echo $rows['paid_amount']; ?></td>
-
-
-                                                            <td>
-                                                                <?php
-                                                                $transaction_action = $rows['action'];
-                                                                $transaction_type = $rows['transaction_type'];
-                                                                
-                                                                if ($transaction_action == 'Recharge' && $transaction_type == '1') {
-                                                                    echo '<span class="badge bg-danger">Recharged</span> <br> <span class="badge bg-success">Paid</span>';
-                                                                } elseif ($transaction_action == 'Recharge' && $transaction_type == '0') {
-                                                                    echo '<span class="badge bg-danger">Recharged </span>';
-                                                                } elseif ($transaction_action == 'paid') {
-                                                                    echo '<span class="badge bg-success">Paid</span>';
-                                                                } elseif ($transaction_action == 'Return') {
-                                                                    echo '<span class="badge bg-warning">Return</span>';
-                                                                }
-                                                                
-                                                                ?>
-                                                            </td>
-                                                            <td>
-                                                                <?php
-                                                                $transaction_type = $rows['transaction_type'];
-                                                                if ($transaction_type == 1) {
-                                                                    echo '<button class="btn-sm btn btn-outline-success">Cash</button>';
-                                                                } elseif ($transaction_type == 0) {
-                                                                    echo '<button class="btn-sm btn btn-outline-danger">Credit</button>';
-                                                                } elseif ($transaction_type == 2) {
-                                                                    echo 'Bkash';
-                                                                } elseif ($transaction_type == 3) {
-                                                                    echo 'Nagad';
-                                                                } elseif ($transaction_type == 4) {
-                                                                    echo 'Bank';
-                                                                } elseif ($transaction_type == 5) {
-                                                                    echo '<button class="btn-sm btn btn-outline-primary">Payment Rcvd</button>';
-                                                                }
-                                                                
-                                                                ?>
-                                                            </td>
-                                                            <td> <?php echo $rows['date']; ?></td>
-                                                        </tr>
-                                                        <?php } ?>
-                                                    </tbody>
-                                                </table>
+                                               <?php  include 'Table/transaction_history_table.php';?>
                                             </div>
                                         </div>
                                     </div>
