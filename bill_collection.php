3c8ccc93c68fe37ada9a5fba750915c23c0dbf67
Rakib Mahmud <fiverrrakib2017@gmail.com>
2025-02-15 17:59:11 +0600

    bill collection issu solve

--- bill_collection.php
+++ bill_collection.php
@@ -14,180 +14,18 @@ include "include/db_connect.php";
     <meta charset="utf-8">
     <title>FAST-ISP-BILLING-SOFTWARE</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
-    <meta content="Themesbrand" name="author">
-
-
-    <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css">
-    <link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css">
-    <!-- C3 Chart css -->
-    <link href="assets/libs/c3/c3.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Bootstrap Css -->
-    <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css">
-    <!-- Icons Css -->
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
-    <!-- App Css-->
-    <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css">
-    <link href="assets/css/custom.css" id="app-style" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="css/toastr/toastr.min.css">
-    <link href="assets/libs/select2/css/select2.min.css" rel="stylesheet" type="text/css">
-    <style>
-        /* .form-inline {
-            display: flex;
-            justify-content: flex-end;
-            margin-bottom: 20px;
-        } */
-    </style>
+    <?php include 'style.php';?>
     </head>
 
     <body data-sidebar="dark">
 
 
-        <!-- Loader -->
-            <div id="preloader"><div id="status"><div class="spinner"></div></div></div>
+       
 
         <!-- Begin page -->
         <div id="layout-wrapper">
         
-            <header id="page-topbar">
-                <div class="navbar-header">
-                    <div class="d-flex">
-                        <!-- LOGO -->
-                        <div class="navbar-brand-box">
-                            <a href="index.php" class="logo logo-dark">
-                                <span class="logo-sm">
-                                    <img src="assets/images/it-fast.png" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="assets/images/it-fast.png" alt="" height="17">
-                                </span>
-                            </a>
-
-                            <a href="index.php" class="logo logo-light">
-                                <span class="logo-sm">
-                                    <img src="assets/images/it-fast.png" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="assets/images/it-fast.png" alt="" height="36">
-                                </span>
-                            </a>
-                        </div>
-
-                        <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect" id="vertical-menu-btn">
-                            <i class="mdi mdi-menu"></i>
-                        </button>
-
-                        <div class="d-none d-sm-block ms-2">
-                            <h4 class="page-title">Bill Collection</h4>
-                        </div>
-                    </div>
-
-                    
-
-                    <div class="d-flex">
-
-                       
-
-                        
-
-                        <div class="dropdown d-none d-md-block me-2">
-                            <button type="button" class="btn header-item waves-effect" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="font-size-16">
-                                    <?php if (isset($_SESSION['fullname'])) {
-                                        echo $_SESSION['fullname'];
-                                    } ?>
-                                </span> 
-                            </button>
-                        </div>
-
-
-                        <div class="dropdown d-inline-block">
-                            <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img class="rounded-circle header-profile-user" src="assets/images/users/avatar-1.jpg" alt="Header Avatar">
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <!-- item-->
-                                <a class="dropdown-item text-danger" href="logout.php">Logout</a>
-                            </div>
-                        </div>
-
-                        <div class="dropdown d-inline-block me-2">
-                            <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="ion ion-md-notifications"></i>
-                                <span class="badge bg-danger rounded-pill">3</span>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0" aria-labelledby="page-header-notifications-dropdown">
-                                <div class="p-3">
-                                    <div class="row align-items-center">
-                                        <div class="col">
-                                            <h5 class="m-0 font-size-16"> Notification (3) </h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div data-simplebar style="max-height: 230px;">
-                                    <a href="" class="text-reset notification-item">
-                                        <div class="d-flex">
-                                            <div class="avatar-xs me-3">
-                                                <span class="avatar-title bg-success rounded-circle font-size-16">
-                                                    <i class="mdi mdi-cart-outline"></i>
-                                                </span>
-                                            </div>
-                                            <div class="flex-1">
-                                                <h6 class="mt-0 font-size-15 mb-1">Your order is placed</h6>
-                                                <div class="font-size-12 text-muted">
-                                                    <p class="mb-1">Dummy text of the printing and typesetting industry.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                    <a href="" class="text-reset notification-item">
-                                        <div class="d-flex">
-                                            <div class="avatar-xs me-3">
-                                                <span class="avatar-title bg-warning rounded-circle font-size-16">
-                                                    <i class="mdi mdi-message-text-outline"></i>
-                                                </span>
-                                            </div>
-                                            <div class="flex-1">
-                                                <h6 class="mt-0 font-size-15 mb-1">New Message received</h6>
-                                                <div class="font-size-12 text-muted">
-                                                    <p class="mb-1">You have 87 unread messages</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                    <a href="" class="text-reset notification-item">
-                                        <div class="d-flex">
-                                            <div class="avatar-xs me-3">
-                                                <span class="avatar-title bg-info rounded-circle font-size-16">
-                                                    <i class="mdi mdi-glass-cocktail"></i>
-                                                </span>
-                                            </div>
-                                            <div class="flex-1">
-                                                <h6 class="mt-0 font-size-15 mb-1">Your item is shipped</h6>
-                                                <div class="font-size-12 text-muted">
-                                                    <p class="mb-1">It is a long established fact that a reader will</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                </div>
-                                <div class="p-2 border-top">
-                                    <div class="d-grid">
-                                        <a class="btn btn-sm btn-link font-size-14  text-center" href="javascript:void(0)">
-                                            View all
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </header>
+           <?php $page_title="Bill Collection"; include 'Header.php';?>
         
             <!-- ========== Left Sidebar Start ========== -->
             <div class="vertical-menu">
@@ -247,8 +85,7 @@ include "include/db_connect.php";
                                         <option value="">Select User</option>
                                         <?php 
                                         /* Fetching users from the database*/
-                                        $userSql = "SELECT id, fullname FROM users";
-                                        $userResult = mysqli_query($con, $userSql);
+                                        $userResult = $con->query("SELECT id, fullname FROM users WHERE user_type='1'");
                                         
                                         while ($userRow = mysqli_fetch_assoc($userResult)) {
                                             echo "<option value='" . $userRow['id'] . "'>" . $userRow['fullname'] . "</option>";
@@ -257,23 +94,21 @@ include "include/db_connect.php";
                                     </select>
                                 </div>
                               <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-    <thead>
-        <tr>
-            <th></th>
-            <th>Recharged Date</th>
-            <th>Collect By</th>
-            <th>Amount</th>
-            <th></th>
-            
-        </tr>
-    </thead>
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Recharged Date</th>
+                                        <th>Collect By</th>
+                                        <th>Amount</th>
+                                        <th></th>
+                                        
+                                    </tr>
+                                </thead>
     <tbody id="tableBody">
     <?php 
-$sql = "SELECT u.id, DATE(cr.datetm) AS recharge_date, SUM(cr.purchase_price) AS total_collection, u.fullname AS recharge_by_name  FROM customer_rechrg cr JOIN users u ON cr.rchg_by = u.id WHERE cr.status = '0' GROUP BY u.id, DATE(cr.datetm), u.fullname ORDER BY recharge_date DESC";
-$result = mysqli_query($con, $sql);
+$result = $con->query("SELECT u.id, DATE(cr.datetm) AS recharge_date, SUM(cr.purchase_price) AS total_collection, u.fullname AS recharge_by_name  FROM customer_rechrg cr JOIN users u ON cr.rchg_by = u.id WHERE cr.status = '0' AND cr.type !='4' GROUP BY u.id, DATE(cr.datetm), u.fullname ORDER BY recharge_date DESC");
 
 while ($rows = mysqli_fetch_assoc($result)) {
-    // Extract year and month from the recharge_date
     $recharge_date = $rows['recharge_date'];
     $yearMonth = date("Y-m-d", strtotime($recharge_date)); 
 ?>
@@ -313,20 +148,7 @@ while ($rows = mysqli_fetch_assoc($result)) {
     </div>
     <!-- End Page-content -->
 
-    <footer class="footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-6">
-                    <script>document.write(new Date().getFullYear())</script> © IT-FAST.
-                </div>
-                <div class="col-sm-6">
-                    <div class="text-sm-end d-none d-sm-block">
-                        Development <i class="mdi mdi-heart text-danger"></i><a href="https://facebook.com/rakib56789">Rakib Mahmud</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include 'Footer.php';?>
 
 </div>
 
@@ -379,89 +201,17 @@ while ($rows = mysqli_fetch_assoc($result)) {
         </div>
         <!-- END layout-wrapper -->
 
-        <!-- Right Sidebar -->
-        <div class="right-bar">
-            <div data-simplebar class="h-100">
-                <div class="rightbar-title px-3 py-4">
-                    <a href="javascript:void(0);" class="right-bar-toggle float-end">
-                        <i class="mdi mdi-close noti-icon"></i>
-                    </a>
-                    <h5 class="m-0">Settings</h5>
-                </div>
-
-                <!-- Settings -->
-                <hr class="mt-0">
-                <h6 class="text-center mb-0">Choose Layouts</h6>
-
-                <div class="p-4">
-                    <div class="mb-2">
-                        <img src="assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="Layouts-1">
-                    </div>
-                    <div class="form-check form-switch mb-3">
-                        <input class="form-check-input theme-choice" type="checkbox" id="light-mode-switch">
-                        <label class="form-check-label" for="light-mode-switch">Light Mode</label>
-                    </div>
-
-                    <div class="mb-2">
-                        <img src="assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="Layouts-2">
-                    </div>
-
-                    <div class="form-check form-switch mb-3">
-                        <input class="form-check-input theme-choice" type="checkbox" id="dark-mode-switch" data-bsStyle="assets/css/bootstrap-dark.min.css" data-appStyle="assets/css/app-dark.min.css">
-                        <label class="form-check-label" for="dark-mode-switch">Dark Mode</label>
-                    </div>
     
-                    <div class="mb-2">
-                        <img src="assets/images/layouts/layout-3.jpg" class="img-fluid img-thumbnail" alt="Layouts-3">
-                    </div>
-
-                    <div class="form-check form-switch mb-3">
-                        <input class="form-check-input theme-choice" type="checkbox"  id="rtl-mode-switch" data-appStyle="assets/css/app-rtl.min.css">
-                        <label class="form-check-label" for="rtl-mode-switch">RTL Mode</label>
-                    </div>
-            
-            
-                </div>
-
-            </div> <!-- end slimscroll-menu-->
-        </div>
-        <!-- /Right-bar -->
 
         <!-- Right bar overlay-->
         <div class="rightbar-overlay"></div>
       
         
         <!-- JAVASCRIPT -->
-    <script src="assets/libs/jquery/jquery.min.js"></script>
-    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/libs/metismenu/metisMenu.min.js"></script>
-    <script src="assets/libs/simplebar/simplebar.min.js"></script>
-    <script src="assets/libs/node-waves/waves.min.js"></script>
-
-    <!-- Required datatable js -->
-    <script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <!-- Buttons examples -->
-    <script src="assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-    <script src="assets/libs/jszip/jszip.min.js"></script>
-    <script src="assets/libs/pdfmake/build/pdfmake.min.js"></script>
-    <script src="assets/libs/pdfmake/build/vfs_fonts.js"></script>
-    <script src="assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
-    <!-- Responsive examples -->
-    <script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-    <script src="js/toastr/toastr.min.js"></script>
-    <script src="js/toastr/toastr.init.js"></script>
-    <!-- Datatable init js -->
-    <script src="assets/js/pages/datatables.init.js"></script>
-
-    <script src="assets/js/app.js"></script>
-    <script src="assets/libs/select2/js/select2.min.js"></script>
+    <?php include 'script.php';?>
     <script type="text/javascript">
         $(document).ready(function(){
+            $("#select_user_id").select2();
             $('#select_user_id').change(function() {
                 var userId = $(this).val();
 
